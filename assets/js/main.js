44ef1ef4732a69de9452bbae05799428d45c03b2
Your Namedmshd <daniel.muyshond@gmail.com>
2018-06-26 15:48:21 +0200

    maj

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,39 @@
 console.log("script loaded");
 
+// var xobj = new XMLHttpRequest();
+// xobj.overrideMimeType("application/json");
+// xobj.open("GET", "assets/json/sequence_info.json", true);
+// xobj.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//         var actual_JSON = JSON.parse(this.responseText);
+//     }
+// };
+// xobj.send(null);
+// let JSON_sequence_info = JSON.parse(xobj.responseText);
+// console.log(JSON_sequence_info);
+
+// var request = new XMLHttpRequest();
+//    request.open("GET", "../../data/file.json", false);
+//    request.send(null)
+//    var my_JSON_object = JSON.parse(request.responseText);
+//    alert (my_JSON_object.result[0]);
+
+// navigator.mediaDevices.getUserMedia(constraints)
+// .then(function(mediaStream) {
+//   var video = document.querySelector('video');
+//   video.srcObject = mediaStream;
+//   video.onloadedmetadata = function(e) {
+//     video.play();
+//   };
+// })
+// .catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
+
+
 //Duree totale de la video
 const getVideoDuration = (video) => {
     return video.duration;
 } 
-let demoVideo = document.getElementById("demovideo");
+let demoVideo = document.getElementById("demovideofrm");
 const demoVideoDuration = getVideoDuration(demoVideo);
 
 //Tous les boutons 
@@ -12,18 +41,21 @@ let videoControlButtons = document.querySelectorAll(".videoControlButton");
 
 //Définition des "blocs" de video
 let bloc1 = {
+    dsc: "1. Préparer 10mm de fil",
     start: 0.0,
     end: 12.0
 };
 
 let bloc2 = {
+    dsc: "2. Procéder à la brasure du composant",
     start: 12,
     end: 27
 };
 
 let bloc3 = {
-  start: 27,
-  end: demoVideoDuration
+    dsc: "3. Procéder au nettoyage",
+    start: 27,
+    end: demoVideoDuration
 };
 
 
@@ -34,19 +66,13 @@ let vcbLength = videoControlButtons.length;
 for (let i=0; i < vcbLength; i++) {
     videoControlButtons[i].addEventListener("click", function (event){
 
-    //checking if it works
     event.preventDefault();
-    console.log("button " + this.id + " active");
-    // let buttonId = this.id;
-    console.log(this.id);
     switch (this.id) {
         case "1":
-        console.log(this.id);
         startCursor = bloc1.start;
         endCursor = bloc1.end;
         break;
         case "2":
-        console.log(this.id);
         startCursor = bloc2.start;
         endCursor = bloc2.end;
         break;
@@ -55,10 +81,8 @@ for (let i=0; i < vcbLength; i++) {
         endCursor = bloc3.end;
         break;
     }
-    console.log(startCursor);
 
-    demovideo.currentTime = startCursor;
-    demovideo.play();
+    demoVideo.currentTime = startCursor;
+    demoVideo.play();
     });
-
 };
---
+++ readme.md
@@ -0,0 +1,32 @@
+# Thales Formation
+
+Une application support pour épauler les candidats dans leur apprentissage de la micro-soudure.
+
+## Heracles, X five, Connexences et Thales Alenia Space, Charleroi
+
+
+## Deploiment
+
+Add additional notes about how to deploy this on a live system
+
+## Built With
+
+* [Dropwizard](http://www.dropwizard.io/1.0.2/docs/) - The web framework used
+* [Maven](https://maven.apache.org/) - Dependency Management
+* [ROME](https://rometools.github.io/rome/) - Used to generate RSS Feeds
+
+
+## Author(s)
+
+* **Daniel Muyshond** - *Initial work* - [dmshd](https://github.com/dmshd)
+
+
+## License
+
+This project is licensed under the Apache license - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Using Bootstrap for starting the project et deliver a template.
+* Using JavaScript to create the functionnality that Jump to a specific time on the video
+* ...
